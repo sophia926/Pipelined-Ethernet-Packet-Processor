cd56aed6f19a3e578177a724d51ef3fc8138f9fa
sophia926 <sophiahytang@gmail.com>
2026-09-13 14:42:01 -0400

    Ethernet parser flush

--- rtl/ethernet_parser.sv
+++ rtl/ethernet_parser.sv
@@ -1,14 +1,12 @@
-module ethernet_parser #(
-    parameter int DATA_WIDTH = 64
-) (
+module ethernet_parser (
     input  logic                       clk,
     input  logic                       rst_n,
 
     // ============================================================
     // Input stream
     // ============================================================
-    input  logic [DATA_WIDTH-1:0]      s_axis_tdata,
-    input  logic [DATA_WIDTH/8-1:0]    s_axis_tkeep,
+    input  logic [63:0]      s_axis_tdata,
+    input  logic [7:0]    s_axis_tkeep,
     input  logic                       s_axis_tvalid,
     output logic                       s_axis_tready,
     input  logic                       s_axis_tlast,
@@ -16,8 +14,8 @@ module ethernet_parser #(
     // ============================================================
     // Output stream
     // ============================================================
-    output logic [DATA_WIDTH-1:0]      m_axis_tdata,
-    output logic [DATA_WIDTH/8-1:0]    m_axis_tkeep,
+    output logic [63:0]      m_axis_tdata,
+    output logic [7:0]    m_axis_tkeep,
     output logic                       m_axis_tvalid,
     input  logic                       m_axis_tready,
     output logic                       m_axis_tlast,
@@ -40,16 +38,42 @@ module ethernet_parser #(
     // Internal state
     // ============================================================
 
-    // TODO:
     // Track our current position within the Ethernet frame.
     typedef enum logic [1:0] {
         ETH_BEAT_0,
         ETH_BEAT_1,
-        ETH_PAYLOAD
+        ETH_PAYLOAD,
+        ETH_FLUSH
     } state_t;
 
     state_t state;
 
+
+    // Need a register than can hold 2 leftover bytes
+    logic [15:0] leftover_data;
+
+    // Other registers
+    logic out_valid_reg;
+    logic [63:0] out_data_reg;
+    logic [7:0] out_keep_reg;
+    logic out_last_reg;
+    logic [3:0] valid_byte_count; // small internal count to decide whether current realigned beat can assert out_last_reg or we need one extra flush beat
+    logic [1:0] flush_byte_count;
+
+    // how many bytes in the current input beat are actually valid
+    always_comb begin
+        case (s_axis_tkeep)
+            8'b0000_0001: valid_byte_count = 4'd1;
+            8'b0000_0011: valid_byte_count = 4'd2;
+            8'b0000_0111: valid_byte_count = 4'd3;
+            8'b0000_1111: valid_byte_count = 4'd4;
+            8'b0001_1111: valid_byte_count = 4'd5;
+            8'b0011_1111: valid_byte_count = 4'd6;
+            8'b0111_1111: valid_byte_count = 4'd7;
+            8'b1111_1111: valid_byte_count = 4'd8;
+            default:      valid_byte_count = 4'd0;
+        endcase
+    end
 
     // ============================================================
     // Handshake logic
@@ -58,22 +82,18 @@ module ethernet_parser #(
     // Determine when input data can be accepted.
     logic input_fire;
     assign input_fire = s_axis_tvalid && s_axis_tready;
-    assign s_axis_tready = m_axis_tready || !out_valid_reg; // Parser can accept a new input beat when either the output register is currently empty or the downstream is ready to consume the current output beat
+    assign s_axis_tready = (state != ETH_FLUSH) && (m_axis_tready || !out_valid_reg); // Parser can accept a new input beat when either the output register is currently empty or the downstream is ready to consume the current output beat
+                                                                                      // But cannot accept when state is ETH_FLUSH
 
 
     // ============================================================
     // Output stream logic
     // ============================================================
 
-    // Need a register than can hold 2 leftover bytes
-    logic [15:0] leftover_data;
-    logic leftover_valid;
-
-    logic out_valid_reg;
-
-    // Ethernet parser is a pass-through stage: inspects the incoming beats to extract Ethernet metadata, but it doesn't modify or remove any bytes
-    // Forward the packet stream toward the IPv4 parser.
     assign m_axis_tvalid = out_valid_reg;
+    assign m_axis_tdata = out_data_reg; 
+    assign m_axis_tkeep = out_keep_reg;
+    assign m_axis_tlast = out_last_reg;
 
 
     // ============================================================
@@ -93,13 +113,21 @@ module ethernet_parser #(
             is_ipv4 <= 1'b0;
             out_valid_reg <= 1'b0;
             leftover_data <= '0;
-            leftover_valid <= 1'b0;
+            out_keep_reg <= '0;
+            out_last_reg <= 1'b0;
+            flush_byte_count <= '0;
+            out_data_reg <= '0;
 
         end
 
         else begin
 
             ethernet_header_valid <= 1'b0; // Make this a one cycle high pulse
+
+            // No replacement was generated, existing output was consumed
+            if (m_axis_tvalid && m_axis_tready) begin
+                out_valid_reg <= 1'b0;
+            end
 
             // Parse Ethernet header beat 0
             if (input_fire && state==ETH_BEAT_0) begin
@@ -142,15 +170,13 @@ module ethernet_parser #(
                 is_ipv4 <= {s_axis_tdata[39:32], s_axis_tdata[47:40]} == ETHERTYPE_IPV4;
 
                 leftover_data <= {s_axis_tdata[55:48], s_axis_tdata[63:56]};
-                leftover_valid <= 1'b1;
 
                 state <= ETH_PAYLOAD;
             end
 
             else if (input_fire && state==ETH_PAYLOAD) begin
-                if (s_axis_tlast) state <= ETH_BEAT_0; // current Ethernet frame has ended
                 
-                m_axis_tdata <= {
+                out_data_reg <= {
                     s_axis_tdata[47:40], // IP7
                     s_axis_tdata[39:32], // IP6
                     s_axis_tdata[31:24],
@@ -164,12 +190,61 @@ module ethernet_parser #(
                 leftover_data <= {s_axis_tdata[55:48], s_axis_tdata[63:56]};
 
                 out_valid_reg <= 1'b1;
+                out_keep_reg <= 8'hFF; // all bytes are valid
+                out_last_reg <= 1'b0; // not the last
+
+                // all remaining packet bytes fit into the final output beat
+                if (s_axis_tlast && valid_byte_count <= 6) begin
+                    out_last_reg <= 1'b1; // last
+                    state <= ETH_BEAT_0;
+
+                    case (valid_byte_count)
+                        4'd0: out_keep_reg <= 8'b0000_0011;
+                        4'd1: out_keep_reg <= 8'b0000_0111;
+                        4'd2: out_keep_reg <= 8'b0000_1111;
+                        4'd3: out_keep_reg <= 8'b0001_1111;
+                        4'd4: out_keep_reg <= 8'b0011_1111;
+                        4'd5: out_keep_reg <= 8'b0111_1111;
+                        4'd6: out_keep_reg <= 8'b1111_1111;
+                    endcase
+
+                end
+
+                else if (s_axis_tlast && valid_byte_count > 6) begin
+                    state <= ETH_FLUSH;
+                    if (valid_byte_count == 4'd7) flush_byte_count <= 2'b01;
+                    else flush_byte_count <= 2'b10;
+                end
+
             end
 
-            // No replacement was generated, existing output was consumed
-            else if (m_axis_tvalid && m_axis_tready) begin
-                out_valid_reg <= 1'b0;
+            else if (state==ETH_FLUSH) begin
+                if (!out_valid_reg || m_axis_tready) begin
+
+                    // Load the final 1-2 leftover bytes
+                    out_data_reg <= {
+                        48'b0,
+                        leftover_data[7:0],
+                        leftover_data[15:8]
+                    };
+
+                    out_valid_reg <= 1'b1;
+                    out_last_reg  <= 1'b1;
+
+                    if (flush_byte_count == 2'd1)
+                        out_keep_reg <= 8'b0000_0001;
+                    else
+                        out_keep_reg <= 8'b0000_0011;
+
+                    // Flush beat is now stored in the output register.
+                    // Parser can prepare for the next Ethernet frame.
+                    state <= ETH_BEAT_0;
+
+                    flush_byte_count <= '0;
+                end
             end
+
+
 
         end
 
